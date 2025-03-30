e6a26722dc6d8af57cbf12f5c6e0a1b9673db9c0
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-03-31 07:12:39 +0900

    chore

--- main.ts
+++ main.ts
@@ -1,41 +1,50 @@
-import { GraphQLClient, gql } from "npm:graphql-request";
+import { Client, gql,cacheExchange, fetchExchange  } from "npm:urql";
 
 const GITHUB_API_URL = "https://api.github.com/graphql";
 const GITHUB_API_TOKEN = Deno.env.get("PAT");
-console.log({GITHUB_API_TOKEN})
 
-const client = new GraphQLClient(GITHUB_API_URL, {
-  headers: {
-    Authorization: `Bearer ${GITHUB_API_TOKEN}`,
+const client =new  Client({
+  url: GITHUB_API_URL,
+  exchanges: [cacheExchange, fetchExchange],
+  fetchOptions: {
+    headers: {
+      Authorization: `Bearer ${GITHUB_API_TOKEN}`,
+    },
   },
 });
 
 async function main() {
   const query = gql`
-query {
-  viewer {
-    contributionsCollection{
-      contributionCalendar {
-        totalContributions
-        weeks {
-          contributionDays {
-            contributionCount
-            date
+    query {
+      viewer {
+        contributionsCollection {
+          contributionCalendar {
+            totalContributions
+            weeks {
+              contributionDays {
+                contributionCount
+                date
+              }
+            }
           }
         }
       }
     }
-  }
-}
   `;
 
   try {
-    const data = await client.request(query);
-    const commitPerDay = data.viewer.contributionsCollection.contributionCalendar.weeks.flatMap(e => e.contributionDays.map(e => [e.contributionCount, e.date]));
+    const result = await client.query(query, {});
 
-    console.log({commitPerDay})
+    if (result.error) {
+      console.error("Error fetching data:", result.error);
+      return;
+    }
+
+    const commitPerDay = result.data.viewer.contributionsCollection.contributionCalendar.weeks.flatMap(e => e.contributionDays.map(e => [e.contributionCount, e.date]));
+
+    console.log({ commitPerDay });
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error("Unexpected error:", error);
   }
 }
 
